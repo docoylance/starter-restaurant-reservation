715206688fe2c4284f4b8702219112eb6e001e73
Lance Docoy <lancedocoy@gmail.com>
2022-03-08 13:12:04 -0900

    Add statement for no tables

--- front-end/src/dashboard/TablesTable.js
+++ front-end/src/dashboard/TablesTable.js
@@ -4,6 +4,7 @@ import ErrorAlert from "../layout/ErrorAlert";
 
 export default function TablesTable({ tables, error }) {
   const history = useHistory();
+
   async function handleClick(reservation_id, table_id) {
     if (
       window.confirm(
@@ -14,29 +15,37 @@ export default function TablesTable({ tables, error }) {
       history.go(0);
     }
   }
-  const tablesList = tables.map(
-    ({ table_id, table_name, capacity, reservation_id }, index) => (
-      <tr key={index}>
-        <td>{table_id}</td>
-        <td>{table_name}</td>
-        <td>{capacity}</td>
-        <td data-table-id-status={table_id}>
-          {reservation_id ? "Occupied" : "Free"}
-        </td>
-        <td>
-          {reservation_id && (
-            <button
-              className="btn btn-primary mr-2"
-              data-table-id-finish={table_id}
-              onClick={() => handleClick(reservation_id, table_id)}
-            >
-              Finish
-            </button>
-          )}
-        </td>
-      </tr>
-    )
+
+  let tablesList = (
+    <tr>
+      <td colSpan={6}>No tables found.</td>
+    </tr>
   );
+  if (tables.length)
+    tablesList = tables.map(
+      ({ table_id, table_name, capacity, reservation_id }, index) => (
+        <tr key={index}>
+          <td>{table_id}</td>
+          <td>{table_name}</td>
+          <td>{capacity}</td>
+          <td data-table-id-status={table_id}>
+            {reservation_id ? "Occupied" : "Free"}
+          </td>
+          <td>
+            {reservation_id && (
+              <button
+                className="btn btn-primary mr-2"
+                data-table-id-finish={table_id}
+                onClick={() => handleClick(reservation_id, table_id)}
+              >
+                Finish
+              </button>
+            )}
+          </td>
+        </tr>
+      )
+    );
+    
   return (
     <div>
       <h4 className="mb-0">Tables</h4>
